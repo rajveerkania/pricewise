807e5e3ee3c92c33ed10e28455dfb470510204bd
ParaagSharma <pspsgxd@gmail.com>
2023-05-04 14:41:16 +0530

    trying

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-const searchInput = document.getElementById("searchInput");
-const search = document.getElementById("searchBar");
-
-function search() {
-  const searchTerm = document.getElementById("searchInput").value;
-  window.location.href = "result.html";
-}
