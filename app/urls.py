3fc977b94ef0d9ed107ed3494336595bd120d3cb
ParaagSharma <pspsgxd@gmail.com>
2023-05-08 12:02:29 +0530

    nothing

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,4 @@ urlpatterns = [
     path('', views.homeView, name = 'index'),
     path('result.html', views.resultView, name = 'result'),
     path('about.html', views.aboutView, name = 'about'),
-    # path('404.html', views.noResultView, name = '404'),
 ]
