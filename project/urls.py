e7533b7956b08f528001b64c20fd3e626adda48f
Rajveer Kania <rajveerkania02@gmail.com>
2023-05-25 14:59:38 +0530

    Updated the urls.py file of the project folder

--- project/urls.py
+++ project/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.static import serve
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
